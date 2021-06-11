5e0431faac8b3a2e73e52a190f9bdfed356d7d49
sergi-reus-code <sergi@reusconstruccions.com>
2021-06-11 07:55:49 +0200

    asd

---
+++ screens/UserDetailScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { View,Text } from "react-native";
+
+
+
+const UserDetailList = () => {
+    return (
+        <View>
+
+            <Text>
+                Putero desde UserDetailList
+            </Text>
+        </View>
+    )
+}
+
+export default UserDetailList
---
+++ screens/UsersList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { View,Text } from "react-native";
+
+
+
+const UsersList = () => {
+    return (
+        <View>
+
+            <Text>
+                Putero desde UserList
+            </Text>
+        </View>
+    )
+}
+
+export default UsersList
